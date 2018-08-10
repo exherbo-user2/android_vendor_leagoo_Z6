3f514f13cf986dd48a96e43485948db32fe53c4c
Alex <a.xarlanov94@gmail.com>
2018-08-10 21:35:49 +0300

    Initial commit

---
+++ Z6-vendor-blobs.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/Z6/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/Z6/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/Z6/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/Z6/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/Z6/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/Z6/vendor,system/vendor)
+
